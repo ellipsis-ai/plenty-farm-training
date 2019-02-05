e9bd09dfcd61e709414b3cbcbe582b4760b00ee0
Luke Andrews <luke@attaboy.ca>
2019-02-05 22:39:00 +0000

    Add an API call to show the full text of the session choices since they're too long for the dropdown

--- data_types/UserTrainingSession/function.js
+++ data_types/UserTrainingSession/function.js
@@ -1,5 +1,7 @@
 function(ellipsis) {
-  const Matrix = require('Matrix');
+  const EllipsisApi = require('ellipsis-api');
+const Matrix = require('Matrix');
+const api = new EllipsisApi(ellipsis).actions;
 const email = ellipsis.event.user.email;
 if (!email) {
   ellipsis.error("No email address available for user while searching for training sessions", {
@@ -10,19 +12,31 @@ if (!email) {
 Matrix.loadData(ellipsis).then((matrix) => {
   const matches = matrix.getAllTrainings(ellipsis.team.timeZone).filter((training) => {
     return training.email === email;
-  }).map((training) => {
-    const label = `${training.formatTopic()}${training.category ? ` (${training.category})` : ""} · ${training.date}`;
-    return {
-      id: training.cellLabel,
-      label: label
-    };
   });
   if (matches.length === 0) {
-    ellipsis.error(`No matching sessions found for email ${email}`, {
+    return ellipsis.error(`No matching sessions found for email ${email}`, {
       userMessage: "No existing training sessions were found for you."
     });
   } else {
-    ellipsis.success(matches);
+    const heading = matches.length === 1 ? "There is 1 training session on file for you:" : `There are ${matches.length} training sessions on file for you:`;
+    const matchList = matches.map((ea, index) => {
+      return `${index + 1}. ${ea.category ? `**${ea.category}**: ` : ""}${ea.formatTopic()} (last completed ${ea.date})`;
+    }).join("\n");
+    const matchResult = matches.map((ea) => {
+      const label = `${ea.formatTopic()} (${ea.date})`;
+      return {
+        id: ea.cellLabel,
+        label: label
+      };
+    });
+    return api.say({
+      message: `${heading}
+${matchList}`
+    }).then(() => {
+      ellipsis.success(matchResult);
+    }).catch(() => { // Ignore errors from say() since it's not strictly necessary
+      ellipsis.success(matchResult);
+    });
   }
 });
 }
